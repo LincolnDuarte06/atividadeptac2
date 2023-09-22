c88002f44f62a18769540286b7dd75eeabf2d289
LincolnDuarte06 <lincolnduartebel@gmail.com>
2023-09-22 10:55:11 -0400

    Finalizando trabalho

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom";
 export default function ToDo() {
    
     return (
-        <div>
-            <h1>Lista de Atividades</h1>
-            <Link to="/todo">TODO</Link>
-           
+        <div className="home-">
+            <h1 className="h1-home">MUNDO DO ANABOLIZANTE</h1>
+            <Link className="link-home" to="/todo">Ir para Fake Natty</Link>
         </div>
     );
 }
--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -6,15 +6,18 @@ export default function ToDo() {
     const [atividade, setAtividade] = useState("");
     const [lista, setLista] = useState([]);
     const [id, setId] = useState(1);
+    const [preco, setPreco] = useState("");
 
     const salvar = (e) => {
         e.preventDefault();
         setLista([...lista, {
             atividade: atividade,
-            id: id
+            id: id,
+            preco:preco
         }]);
         setId(id + 1);
         setAtividade("");
+        setPreco("");
     };
     const remover = (id) => {
         /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
@@ -28,22 +31,31 @@ export default function ToDo() {
     }
     return (
         <div class="container">
-            <Link to="/">home</Link>
+            
             <h1>Lista de Atividades</h1>
             <form onSubmit={salvar}>
                 <input type="text"
                     value={atividade}
                     onChange={(e) => { setAtividade(e.target.value) }} />
+
+                    <input type="text"
+                    value={preco}
+                    onChange={(e) => { setPreco(e.target.value) }} />
+                    <br></br>
                 <button>ADICIONAR</button>
             </form>
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
                     <li>
-                        <p>{ativ.atividade}</p>
+                        <p className="p1">{ativ.id} {ativ.atividade} </p>
+                        <p className="p">{ativ.preco}</p>
+                        
                         <button onClick={() => remover(ativ.id)}>Remover</button>
                     </li>
                 </ul>
             )}
+            <Link className="botao-home" to="/">Voltar a ser natural</Link>
         </div>
+        
     );
 }
